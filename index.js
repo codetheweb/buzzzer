7116156f9482915aa5f93edc752fd0cf4e9fb177
Max Isom <code@maxisom.me>
2019-02-23 14:40:50 -0500

    Move to Express and Socket.io

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const path = require('path');
+const sassMiddleware = require('node-sass-middleware')
+
+io.on('connection', socket => {
+  // Client wants to join room
+  socket.on('room', request => {
+    const {room, playerId} = request;
+
+    // Let client know if they're the host
+    io.in(room).clients((error, clients) => {
+      const numOfClients = clients.length;
+
+      socket.emit('host', (numOfClients === 0));
+    })
+
+    // Join the client to the room
+    socket.join(room);
+  });
+
+  // Client buzzed in
+  socket.on('buzzed', request => {
+    const room = Object.keys(socket.rooms)[1];
+
+    // Emit to all sockets in room
+    io.sockets.in(room).emit('buzzed', request);
+  });
+
+  // Host reset game
+  socket.on('reset', () => {
+    const room = Object.keys(socket.rooms)[1];
+    
+    io.sockets.in(room).emit('reset');
+  })
+});
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: false, // true = .sass and false = .scss
+  sourceMap: true,
+  includePaths: ['node_modules']
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/pages/index.html');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
---
+++ public/javascripts/main.js
@@ -0,0 +1,58 @@
+const socket = io();
+let room, user, timeAtReset;
+
+// Emitted when self joins
+socket.on('host', isHost => {
+  $('.welcome').addClass('hidden');
+
+  if (isHost) {
+    $('.host').removeClass('hidden');
+  } else {
+    $('.player').removeClass('hidden');
+  }
+});
+
+// Emitted when player buzzes in
+socket.on('buzzed', data => {
+  // Play buzzer sound effect
+  document.getElementById('buzzer-sound-effect').play();
+
+  // Show results
+  $('.results .playerId').text(data.playerId);
+  $('.results .buzzTime').text(data.time / 1000);
+
+  // Disable buzz button
+  $('#buzz-button').removeClass('enabled').attr('disabled', 'disabled');
+});
+
+// Emitted when game is reset
+socket.on('reset', () => {
+  // Enable buzz button
+  $('#buzz-button').addClass('enabled').removeAttr('disabled');
+
+  // Clear results
+  $('.results .playerId').text('...');
+  $('.results .buzzTime').text('...');
+
+  // Reset timer
+  timeAtReset = + new Date();
+});
+
+$('#join-form').submit(e => {
+  e.preventDefault();
+
+  room = e.target.room.value;
+  user = e.target.user.value;
+
+  socket.emit('room', {room, user});
+});
+
+$('#buzz-button').click(e => {
+  const time = new Date() - timeAtReset;
+
+  socket.emit('buzzed', {playerId: user, time: time})
+});
+
+$('#host-reset').click(e => {
+  socket.emit('reset');
+});
